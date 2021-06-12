86e3f0a99c58ff97be96a1d5be7aae80976faf70
[Andrew Core] <[adcore89@gmail.com]>
2021-06-12 13:19:44 -0400

    catching up on unhanded in project, rejigging JS for questions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,82 +1,69 @@
-
 //elements and event listeners
 
-var startButton = document.getElementById("start-btn")
-var nextButton = document.getElementById("next-btn")
-var questionContainerElement = document.getElementById("question-container")
-var questionElement= document.getElementById("question")
-var answerButtonElement = document.getElementById("answer-buttons")
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerElement = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var answerButtonElement = document.getElementById("answer-buttons");
+var randomQuestions, currentQuestionIndex;
 
+//questions array
+var questions = [
+  {
+    question: "<h1>what does the 'C' in CSS stand for?</h1>",
+    answers: [
+      { text: "Cascading", correct: true },
+      { test: "Canadian", correct: false },
+      { test: "Computational", correct: false },
+      { test: "Colourful", correct: false },
+    ],
+  },
+  {
+    question: "<h1>Which of the following is a 3rd party API</h1>",
+    answers: [
+      { text: "bootstrap", correct: true },
+      { text: "shoelace", correct: false },
+      { text: " cup of java", correct: false },
+      { text: " javanator", correct: false },
+    ],
+  },
+  {
+    question: "<h1>what is a boolean</h1>",
+    answers: [
+      { text: "a value that that is only true", correct: false },
+      { text: "a cube to make soup", correct: false },
+      { text: "a value that is only true or false", correct: true },
+      { text: " a made up word", correct: false },
+    ],
+  },
+  {
+    question: "<h1> what does DOM stand for</h1>",
+    answers: [
+      { text: " Document Object Maker", correct: false },
+      { text: "Document Object Model", correct: true },
+      { text: " Design Object Modulator", correct: false },
+      {text: " A character in the Fast and Furious franchise", correct: false, },
+     ],
+  },
+];
 
-startButton.addEventListener("click", startQuiz);
-nextButton.addEventListener("click", nextQuestion)
-
-
-var randomQuestions, currentQuestionIndex
-
-// start the game 
-function startQuiz() {
-    var startButton = document.getElementById("start-btn");
-    startButton.remove();
-    randomQuestions = question.sort(()=> Math,random() - .5)
-    currentQuestionsIndex = 0
-    nextQuestion()    
+// start the game
+var startQuize = function () {
+  var startButton = document.getElementById("start-btn");
+  startButton.remove();
+  
+  nextQuestion();
 }
-// set question 
+// set question
 function nextQuestion() {
-showQuestion    
-   
+  showQuestion;
 }
 
 // show question from array
 function showQuestion(question) {}
-questionElement.innertext = question.question
+questionElement.innertext = question.question;
 
-function answers() {
+function answers() {}
 
-}
-
-//questions array 
-var questions = [
-
-    {
-        question: "what does the 'C' in CSS stand for?",
-        answers: {
-            a: "Cascading",
-            b: "Canadian",
-            c: "Computational",
-            d: "Colourful",
-        },
-        correctAnswer: "a"
-    },
-    {
-    question: "Which of the following is a 3rd party API",
-    answers: {
-        a: "Shoelace",
-        b: "Wellington",
-        c: "Javaflava",
-        d: "Bootstrap",
-    },
-    correctAnswer: "d"
-},
-{
-    question: "what is a boolean",
-    answers: {
-        a: "a value that that is only true",
-        b: "a cube to make soup",
-        c: "a value that is only true or false",
-        d: "a made up word",
-    },
-    correctAnswer: "c"
-},
-{   
-question: "what does 'DOM' standfor",
-answers: {
-    a: "Document Object Maker",
-    b: "Document Object Model",
-    c: "Design Object Modulator",
-    d: "a character from the Fast and the Furious franchise ",
-},
-correctAnswer: "d"     
-}
-]
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", nextQuestion);
