af0d8bb764fc9389a0f10edf14ffcc287d55e0e2
[Andrew Core] <[adcore89@gmail.com]>
2021-06-13 10:40:42 -0400

    quiz will cycle thru array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,16 @@
 //elements and event listeners
 
-var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn");
-var questionContainerElement = document.getElementById("question-container");
-var questionElement = document.getElementById("question");
-var answerButtonElement = document.getElementById("answer-buttons");
-var randomQuestions, currentQuestionIndex;
+var startButton = document.querySelector("#start");
+var answersList = document.querySelector("#choices");
+var questionsDiv = document.querySelector(".questions");
+var questionElement = document.querySelector("#question-container");
+var rightAnswer = document.querySelector("#correct");
+var wrongAnswer = document.querySelector("#incorrect");
+var quizEnd = document.querySelector("#finish");
+var answerButtonElement = document.querySelector("answer-buttons");
+var activeTimer = document.querySelector("#timer");
+var timeRemaining = 70;
+var displayQuestions, questionIndex;
 
 //questions array
 var questions = [
@@ -13,9 +18,9 @@ var questions = [
     question: "<h1>what does the 'C' in CSS stand for?</h1>",
     answers: [
       { text: "Cascading", correct: true },
-      { test: "Canadian", correct: false },
-      { test: "Computational", correct: false },
-      { test: "Colourful", correct: false },
+      { text: "Canadian", correct: false },
+      { text: "Computational", correct: false },
+      { text: "Colourful", correct: false },
     ],
   },
   {
@@ -30,7 +35,7 @@ var questions = [
   {
     question: "<h1>what is a boolean</h1>",
     answers: [
-      { text: "a value that that is only true", correct: false },
+      { text: "a value that is only true", correct: false },
       { text: "a cube to make soup", correct: false },
       { text: "a value that is only true or false", correct: true },
       { text: " a made up word", correct: false },
@@ -42,28 +47,92 @@ var questions = [
       { text: " Document Object Maker", correct: false },
       { text: "Document Object Model", correct: true },
       { text: " Design Object Modulator", correct: false },
-      {text: " A character in the Fast and Furious franchise", correct: false, },
-     ],
+      {
+        text: " A character in the Fast and Furious franchise",
+        correct: false,
+      },
+    ],
   },
 ];
 
-// start the game
-var startQuize = function () {
-  var startButton = document.getElementById("start-btn");
-  startButton.remove();
-  
-  nextQuestion();
-}
-// set question
-function nextQuestion() {
-  showQuestion;
-}
+var startGame = function () {
+  startButton.setAttribute("class", "hide");
+  displayQuestions = questions;
+  questionIndex = 0;
+  quizEnd.setAttribute("class", "hide")
+  startQuiz();
+};
+
+var startQuiz = function () {
+  reset();
+  showQuestions(displayQuestions[questionIndex]);
+};
 
 // show question from array
-function showQuestion(question) {}
-questionElement.innertext = question.question;
+var showQuestions = function (question) {
+  questionElement.innerHTML = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.setAttribute("class", "btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", answerChoice);
+    answersList.appendChild(button);
+  });
+};
 
-function answers() {}
+var reset = function () {
+  while (answersList.firstChild) {
+    answersList.removeChild(answersList.firstChild);
+  }
+};
 
-startButton.addEventListener("click", startQuiz);
-nextButton.addEventListener("click", nextQuestion);
+var answerChoice = function (event) {
+  var answerSelect = event.target;
+  var correct = answerSelect.dataset.correct;
+  if (displayQuestions.length > questionIndex + 1) {
+    displayAnswer(rightAnswer, wrongAnswer, correct);
+  } else {
+    clearDisplayAnswer(rightAnswer, wrongAnswer);
+    questionsDiv.setAttribute("class", "hide");
+    quizEnd.removeAttribute("class", "hide");
+  }
+  questionIndex++;
+  startQuiz();
+};
+
+//print answer
+var displayAnswer = function (rightAnswer, wrongAnswer, correct) {
+  clearDisplayAnswer(rightAnswer, wrongAnswer);
+  if (correct) {
+    rightAnswer.removeAttribute("class", "hide");
+  } else {
+    wrongAnswer.removeAttribute("class", "hide");
+    timeRemaining -= 5;
+  }
+};
+
+var clearDisplayAnswer = function (rightAnswer, wrongAnswer) {
+  rightAnswer.setAttribute("class", "hide");
+  wrongAnswer.setAttribute("class", "hide");
+};
+
+var quizTimer = function () {
+  activeTimer = activeTimer;
+  var timeInterval = setInterval(function () {
+    if (timeRemaining > 0 && quizEnd.hasAttribute("class", "hide")) {
+      activeTimer.textContent = "time:" + timeRemaining;
+      timeRemaining--;
+    } else {
+      clearInterval(timeInterval);
+      clearDisplayAnswer(rightAnswer, wrongAnswer);
+      questionsDiv.setAttribute("class", "hide");
+      localStorage.setItem("recentScore", timeRemaining);
+    }
+  }, 1000);
+};
+
+startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", quizTimer);
